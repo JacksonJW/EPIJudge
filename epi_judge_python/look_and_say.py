9e1f8da0ab9afc3688165f1306e4fa8be261120b
JacksonJW <jacksonjwatkins@gmail.com>
2019-09-30 16:40:02 -0700

    EPI 6.7

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -3,7 +3,30 @@ from test_framework import generic_test
 
 def look_and_say(n):
     # TODO - you fill in here.
-    return ''
+    if n < 1:
+        return ""
+    prev_str = "1"
+    new_str = ""
+    look_say_table = {}
+    look_say_index = 1
+    while look_say_index < n:
+        i = 0
+        j = 0
+        while j < len(prev_str):
+            while j < len(prev_str) and prev_str[j] == prev_str[i]:
+                j += 1
+            look = prev_str[i:j]
+            if look in look_say_table:
+                new_str += look_say_table[look]
+            else:
+                say = str(len(look)) + prev_str[i]
+                look_say_table[look] = say
+                new_str += say
+            i = j
+        prev_str = new_str
+        new_str = ""
+        look_say_index += 1
+    return prev_str
 
 
 if __name__ == '__main__':
--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -3,8 +3,22 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n):
-    # TODO - you fill in here.
-    return []
+    if n < 2:
+        return []
+    primes = []
+    for num in range(2, n+1):
+        if not primes:
+            primes.append(num)
+        else:
+            prime_index = 0
+            while True:
+                if num % primes[prime_index] == 0:
+                    break
+                if prime_index == len(primes)-1:
+                    primes.append(num)
+                    break
+                prime_index += 1
+    return primes
 
 
 if __name__ == '__main__':
