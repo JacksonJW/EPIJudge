ce9499593470b81560fadf1f5b7da81b0faaebc3
JacksonJW <jacksonjwatkins@gmail.com>
2019-10-05 22:50:26 -0700

    inorder traversal manipulating the tree in place

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    result = []
+    if tree is None or tree.data is None:
+        return result
+    node_ptr = tree
+    while True:
+        while node_ptr.left and node_ptr.left.data is not None:
+            node_ptr = node_ptr.left
+        result.append(node_ptr.data)
+        node_ptr.data = None
+        if node_ptr.right and node_ptr.right.data is not None:
+            node_ptr = node_ptr.right
+            continue
+        else:
+            while node_ptr.parent and node_ptr.data is None:
+                node_ptr = node_ptr.parent
+            if node_ptr.data is not None:
+                continue
+            else:
+                break
+    return result
 
 
 if __name__ == '__main__':
