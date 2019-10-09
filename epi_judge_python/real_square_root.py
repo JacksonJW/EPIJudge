5fbcda5b1e7ef0695e64f4ae58c0316c57ff36f0
JacksonJW <jacksonjwatkins@gmail.com>
2019-10-08 18:40:57 -0700

    real_square_root completed

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
+import math
 
 
 def square_root(x):
-    # TODO - you fill in here.
-    return 0.0
+    lower_bound, upper_bound = (x, 1.0) if x < 1 else (1.0, x)
+
+    while not math.isclose(lower_bound, upper_bound):
+        median = lower_bound + (upper_bound - lower_bound) * 0.5
+        if median * median > x:
+            upper_bound = median
+        else:
+            lower_bound = median
+
+    return lower_bound
 
 
 if __name__ == '__main__':
